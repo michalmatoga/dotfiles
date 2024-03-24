f672c3eee9d95e117eac8e7307f7708c3b45aac9
michmato <michalmatoga@gmail.com>
2024-03-24 08:35:34 +0000

    chore: update configuration

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -8,13 +8,15 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [
-    # include NixOS-WSL modules
-    <nixos-wsl/modules>
-  ];
-
   wsl.enable = true;
   wsl.defaultUser = "nixos";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+    curl
+  ];
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -23,14 +25,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-  
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  environment.systemPackages = with pkgs; [
-    # Flakes clones its dependencies through the git command,
-    # so git must be installed first
-    git
-    vim
-    wget
-    curl
-  ];
 }
