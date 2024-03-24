fc915456339e0490b71ca79bb8e599d7cfeae75a
michmato <michalmatoga@gmail.com>
2024-03-24 08:31:17 +0000

    chore: update flake

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -1,20 +1,18 @@
 {
-  description = "A simple NixOS flake";
-
+  description = "A very basic flake";
+  
   inputs = {
-    # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixos-wsl.url = "github:nix-community/nixos-wsl";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
+  outputs = { self, nixpkgs, nixos-wsl }: {
+     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+       system = "x86_64-linux";
+       modules = [
         ./configuration.nix
-      ];
-    };
+        nixos-wsl.nixosModules.wsl
+       ];
+     };
   };
 }
