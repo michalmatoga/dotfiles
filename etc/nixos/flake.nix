fdd90fd33163bcc429b2fba10fd02f44a5e50a9d
michmato <michalmatoga@gmail.com>
2024-03-24 08:24:40 +0000

    chore: change system

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -8,8 +8,8 @@
 
   outputs = { self, nixpkgs, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
