28dade80ea7418cfe2f100d1f1d0b8b1050a1276
michmato <michalmatoga@gmail.com>
2024-03-27 10:05:46 +0000

    chore: update config

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-node $DIR/scripts/sync-repos.js
+node ~/.config/dotfiles/scripts/sync-repos.js
 sudo nixos-rebuild switch
