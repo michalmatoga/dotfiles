c3e78c7520275208401361cfefb6e8d8d22c4200
michmato <michalmatoga@gmail.com>
2024-03-27 09:26:28 +0000

    chore: update config

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -101,7 +101,7 @@
       plugins = [
         { name = "zsh-users/zsh-autosuggestions"; }
         # { name = "jeffreytse/zsh-vi-mode"; }
-        # { name = "joshskidmore/zsh-fzf-history-search"; }
+        { name = "joshskidmore/zsh-fzf-history-search"; }
         { name = "unixorn/fzf-zsh-plugin"; }
       ];
     };
