e9d6b1a586db84cc2581b3e498552ca3c09d4498
Michał Matoga <michalmatoga@gmail.com>
2024-03-28 12:10:31 +0000

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -161,8 +161,6 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('i', 'jj', '<Esc>')
 
-
-
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
