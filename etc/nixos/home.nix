6e8f355d61b27c713d1832e47261948141d8c0a2
Michał Matoga <michalmatoga@gmail.com>
2024-03-29 07:18:31 +0000

    chore: update config

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -43,7 +43,7 @@
     yq-go # yaml processor https://github.com/mikefarah/yq
     eza # A modern replacement for ‘ls’
     fzf # A command-line fuzzy finder
-		tmux
+    tmux
     tmuxinator
     xdg-utils
     gnumake
