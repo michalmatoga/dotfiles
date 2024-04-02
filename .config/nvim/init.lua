abef8cd025023dcf047510b30d0fb3553bca9644
Michał Matoga <michalmatoga@gmail.com>
2024-04-02 08:24:47 +0000

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -247,7 +247,11 @@ require('lazy').setup({
     "folke/flash.nvim",
     event = "VeryLazy",
     -- @type Flash.Config
-    opts = {},
+    opts = {
+      label = {
+        uppercase = false,
+      },
+    },
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
