40d213f1b04aa46db67dca1ebd99452b26fff8d9
Michał Matoga <michalmatoga@gmail.com>
2024-04-03 09:08:30 +0000

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -637,6 +637,7 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
+        json = { { "prettierd", "prettier" } },
         javascript = { { "prettierd", "prettier" } },
         typescript = { { "prettierd", "prettier" } },
         markdown = { "prettierd", "prettier" },
