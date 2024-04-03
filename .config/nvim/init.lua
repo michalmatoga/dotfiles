79aea83ac8e3ee2db5c4c8e1ef4fef93c1af2b0f
Michał Matoga <michalmatoga@gmail.com>
2024-04-03 08:15:57 +0000

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -89,6 +89,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_user_command("CopyRelPath", "call setreg('+', expand('%'))", {})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
