30cc41a3a667f0a3529557fc2b09a16eba994694
michmato <michalmatoga@gmail.com>
2024-04-05 10:31:27 +0000

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -192,8 +192,13 @@ require('lazy').setup({
       }
     end,
   },
+  -- {
+  -- 'rmagatti/auto-session', opts = {},
+  -- },
   {
-  'rmagatti/auto-session', opts = {},
+    "olimorris/persisted.nvim",
+    lazy = false, -- make sure the plugin is always loaded at startup
+    config = true
   },
   {
     'sindrets/diffview.nvim',
