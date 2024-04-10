2345505530555f885774c3df3906dffc2196a289
michmato <michalmatoga@gmail.com>
2024-04-10 19:01:32 +0200

    chore: update config

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -112,8 +112,8 @@
       clip = "clip.exe";
       cplc = "history | tail -n 1 | cut -d' ' -f5- | clip.exe";
       cpwd = "pwd | tr -d '\n' | clip.exe";
-      nx = "npx -y nx";
       import-sort-cli = "npx -y import-sort-cli";
+      nx = "npx -y nx";
       paste = "powershell.exe get-clipboard";
       pro = "tmuxinator start $(tmuxinator list --newline | fzf)";
       sync-repos = "node ~/ghq/github.com/michalmatoga/dotfiles/scripts/sync-repos.mjs";
