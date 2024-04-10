c07528da450a35fa6c5d667366ebae468ba2c499
michmato <michalmatoga@gmail.com>
2024-04-10 18:32:29 +0200

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -978,6 +978,12 @@ require('lazy').setup({
       })
     end,
   },
+  {
+      "andrewferrier/wrapping.nvim",
+      config = function()
+          require("wrapping").setup()
+      end
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
