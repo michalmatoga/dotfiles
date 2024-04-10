8f78776acb1a2e5c08a808785afd3d22bb953c80
michmato <michalmatoga@gmail.com>
2024-04-10 19:31:50 +0200

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -975,9 +975,9 @@ require('lazy').setup({
       vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
           desc = "Search current word"
       })
-      vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-          desc = "Search on current file"
-      })
+      -- vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+      --     desc = "Search on current file"
+      -- })
     end,
   },
   {
