0af62b3d17ca83fc5dc08c5e38c9c72d8567f9f6
michmato <michalmatoga@gmail.com>
2024-04-11 09:43:49 +0200

    chore: update config

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -33,7 +33,7 @@
 
   virtualisation.docker.enable = true;
 
-  services.automatic-timezoned.enable = true;
+  # services.automatic-timezoned.enable = true;
   time.timeZone = lib.mkDefault "Europe/Warsaw";
 
   system.stateVersion = "23.11";
