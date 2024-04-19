311d6f8f889196f1bd1a5eee686e21e7f190f645
michmato <michalmatoga@gmail.com>
2024-04-19 08:26:22 +0200

    chore: update config

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -26,6 +26,7 @@
     };
   };
 
+  programs.nix-ld.enable = true;
   programs.zsh.enable = true;
 
   users.defaultUserShell = pkgs.zsh;
--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -123,6 +123,8 @@
     initExtra = ''
       export BROWSER="wsl-open"
       export GH_USER="michalmatoga"
+
+      export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
     '';
   };
   programs.neovim = {
