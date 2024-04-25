57d0df711e3f438e5022ed0a7288d81c9c324bb6
michmato <michalmatoga@gmail.com>
2024-04-25 08:19:48 +0200

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -892,6 +892,36 @@ require('lazy').setup({
         folder = 'journal',
         template = 'daily-note.md',
       },
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ['gf'] = {
+          action = function()
+            return require('obsidian').util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes.
+        ['<leader>ch'] = {
+          action = function()
+            return require('obsidian').util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        -- Smart action depending on context, either follow link or toggle checkbox.
+        ['<cr>'] = {
+          action = function()
+            return require('obsidian').util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
+        ['<leader>nn'] = {
+          action = function()
+            return require('obsidian').commands.link_new()
+          end,
+          opts = { buffer = true, expr = true },
+        },
+      },
+
       -- Optional, customize how note IDs are generated given an optional title.
       ---@param title string|?
       ---@return string
