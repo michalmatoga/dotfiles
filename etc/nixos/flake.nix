a8fee16b10ecda501e00d1fe6ec9c2d37b94d9a8
michmato <michalmatoga@gmail.com>
2024-04-26 09:31:12 +0200

    chore: update config

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -31,7 +31,7 @@
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
         home-manager.users.nixos = import ./home.nix;
-        home-manager.extraSpecialArgs = { inherit unstable };
+        home-manager.extraSpecialArgs = { inherit unstable; };
       }
      ];
    };
