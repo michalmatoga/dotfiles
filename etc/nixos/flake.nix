5bd96bc7b7f65656b647788620253f9c5547be11
michmato <michalmatoga@gmail.com>
2024-04-26 09:30:31 +0200

    chore: update config

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -11,29 +11,29 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-wsl, home-manager }: {
-    let
-      pkgs = import nixpkgs {inherit system;}
-      unstable = import nixpkgs-unstable {inherit system;}
-    in
-    {
-     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-       system = "x86_64-linux";
-       # unstable = import nixpkgs-unstable { inherit system };
-       # specialArgs = { inherit unstable; };
-       modules = [
-        ./configuration.nix
-        nixos-wsl.nixosModules.wsl
-        # make home-manager as a module of nixos
-        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.nixos = import ./home.nix;
-          home-manager.extraSpecialArgs = { inherit unstable };
-        }
-       ];
-     };
-    };
-}
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-wsl, home-manager }:
+  let
+    pkgs = import nixpkgs {inherit system;}
+    unstable = import nixpkgs-unstable {inherit system;}
+  in
+  {
+   nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+     system = "x86_64-linux";
+     # unstable = import nixpkgs-unstable { inherit system };
+     # specialArgs = { inherit unstable; };
+     modules = [
+      ./configuration.nix
+      nixos-wsl.nixosModules.wsl
+      # make home-manager as a module of nixos
+      # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.nixos = import ./home.nix;
+        home-manager.extraSpecialArgs = { inherit unstable };
+      }
+     ];
+   };
+  };
+
