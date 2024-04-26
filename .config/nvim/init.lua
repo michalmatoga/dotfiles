14ae50dad35981de376e6bea045f5da4debf0649
michmato <michalmatoga@gmail.com>
2024-04-26 17:26:36 +0200

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -595,6 +595,12 @@ require('lazy').setup({
           vim.diagnostic.open_float(nil, { focus = false })
         end,
       })
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        group = vim.api.nvim_create_augroup('float_diagnostic_cursor', { clear = true }),
+        callback = function()
+          vim.diagnostic.open_float(nil, { focus = false, scope = 'cursor' })
+        end,
+      })
       vim.diagnostic.config {
         virtual_text = false,
       }
