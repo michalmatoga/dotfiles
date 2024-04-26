cbe1a11604d17b2cb33a162dce3c65fcb10cddfb
michmato <michalmatoga@gmail.com>
2024-04-26 17:21:38 +0200

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -586,6 +586,10 @@ require('lazy').setup({
         end,
       })
 
+      vim.diagnostic.config {
+        virtual_text = false,
+      }
+
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
