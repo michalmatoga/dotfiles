fd7534c25c1aca7463309dea89186087641f9989
michmato <michalmatoga@gmail.com>
2024-04-26 17:22:48 +0200

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -586,6 +586,15 @@ require('lazy').setup({
         end,
       })
 
+      -- You will likely want to reduce updatetime which affects CursorHold
+      -- note: this setting is global and should be set only once
+      vim.o.updatetime = 250
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        group = vim.api.nvim_create_augroup('float_diagnostic', { clear = true }),
+        callback = function()
+          vim.diagnostic.open_float(nil, { focus = false })
+        end,
+      })
       vim.diagnostic.config {
         virtual_text = false,
       }
