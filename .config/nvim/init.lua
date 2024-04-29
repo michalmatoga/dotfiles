ae681704359b5ae7bf23ff035e9c135eeb30f684
michmato <michalmatoga@gmail.com>
2024-04-29 08:58:42 +0200

    chore: update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -875,7 +875,7 @@ require('lazy').setup({
     'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'catppuccin-frappe'
+      vim.cmd.colorscheme 'catppuccin-mocha'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
