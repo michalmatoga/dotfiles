56943af081fd27da9ad9d818b982b89d94c8fcd2
michmato <michalmatoga@gmail.com>
2024-05-06 07:17:14 +0200

    chore: update config

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -146,6 +146,7 @@ in
 
       export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
       export PATH="$HOME/.cache/npm/global/bin:$PATH"
+      eval "$(fnm env --use-on-cd)"
     '';
   };
   programs.neovim = {
