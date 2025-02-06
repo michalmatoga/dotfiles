dee4c91a2880552f585d30be490d1887ce0b57b5
michmato <michalmatoga@gmail.com>
2025-02-06 13:52:41 +0100

    chore: update keymap descriptions for Telescope commands

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -736,11 +736,11 @@ require('lazy').setup({
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sc', builtin.command_history, { desc = '[S]earch [C]ommands History' })
-      vim.keymap.set('n', '<leader>ss', builtin.git_status, { desc = '[S]earch [C]hanged Files' })
+      vim.keymap.set('n', '<leader>ss', builtin.git_status, { desc = '[S]earch Git [Status]' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>st', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>st', builtin.builtin, { desc = '[S]earch Select [T]elescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
