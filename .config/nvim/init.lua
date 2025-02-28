53647fa825011f565ccf67d00adb437854896d9f
michmato <michalmatoga@gmail.com>
2025-02-28 14:23:05 +0100

    chore: add dockerls configuration to Neovim LSP setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -959,6 +959,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
+        dockerls = {},
         jsonls = {},
         lua_ls = {
           -- cmd = {...},
