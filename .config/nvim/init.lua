a9bc91a46bdd23fa8a599d0ad2ad5ebcef139a3c
michmato <michalmatoga@gmail.com>
2025-03-02 09:08:57 +0100

    chore: remove unused dockerls configuration from Neovim setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -959,7 +959,6 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
-        dockerls = {},
         jsonls = {},
         lua_ls = {
           -- cmd = {...},
