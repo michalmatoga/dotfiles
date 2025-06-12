6e8e0a7ef6e0d313c066c84f63cf0d8ada4c3a6c
michmato <michalmatoga@gmail.com>
2025-06-12 10:27:59 +0200

    feat: add a completion message to the nagging script output

--- scripts/nag.sh
+++ scripts/nag.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo -e "\n\nDone! Get over here! (ctrl + c to interrupt nagging process)"
 while true; do powershell.exe -c "(New-Object Media.SoundPlayer 'C:\\Windows\\Media\\tada.wav').PlaySync();"; sleep 0.5; done
