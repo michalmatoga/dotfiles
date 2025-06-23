a47ac04e377d4ce1ae0102f2f78dd741ab7feb13
michmato <michalmatoga@gmail.com>
2025-06-23 16:43:01 +0200

    chore: comment out example configuration options in avante.lua

--- .config/lazyvim/lua/plugins/avante.lua
+++ .config/lazyvim/lua/plugins/avante.lua
@@ -14,20 +14,20 @@ return {
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     opts = {
-      -- add any opts here
-      -- for example
-      provider = "claude",
-      providers = {
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          model = "claude-sonnet-4-20250514",
-          timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 20480,
-          },
-        },
-      },
+      -- -- add any opts here
+      -- -- for example
+      -- provider = "claude",
+      -- providers = {
+      --   claude = {
+      --     endpoint = "https://api.anthropic.com",
+      --     model = "claude-sonnet-4-20250514",
+      --     timeout = 30000, -- Timeout in milliseconds
+      --     extra_request_body = {
+      --       temperature = 0.75,
+      --       max_tokens = 20480,
+      --     },
+      --   },
+      -- },
     },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
