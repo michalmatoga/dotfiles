3e8bdf88c950eded03d963f4d66dcc335e2cc8ea
michmato <michalmatoga@gmail.com>
2025-06-23 16:46:27 +0200

    chore: update OpenAI configuration in Avante plugin settings

--- .config/lazyvim/lua/plugins/avante.lua
+++ .config/lazyvim/lua/plugins/avante.lua
@@ -14,20 +14,17 @@ return {
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     opts = {
-      -- -- add any opts here
-      -- -- for example
-      -- provider = "claude",
-      -- providers = {
-      --   claude = {
-      --     endpoint = "https://api.anthropic.com",
-      --     model = "claude-sonnet-4-20250514",
-      --     timeout = 30000, -- Timeout in milliseconds
-      --     extra_request_body = {
-      --       temperature = 0.75,
-      --       max_tokens = 20480,
-      --     },
-      --   },
-      -- },
+      -- add any opts here
+      -- for example
+      provider = "openai",
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0,
+        max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      },
     },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
