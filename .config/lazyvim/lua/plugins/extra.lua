5cbe1bea5d44eb22b9c9ab9cc463eee45a55bb88
michmato <michalmatoga@gmail.com>
2025-06-23 12:17:16 +0200

    chore: add configuration options for hidden files in snacks.nvim picker

--- .config/lazyvim/lua/plugins/extra.lua
+++ .config/lazyvim/lua/plugins/extra.lua
@@ -19,11 +19,14 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      explorer = {
-        hidden = true,
-        -- your explorer configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+      picker = {
+        sources = {
+          explorer = {
+            files = {
+              hidden = true,
+            },
+          },
+        },
       },
     },
   },
