fde6fd7db0cb0a4b1abb69deba243c5b54b5d897
michmato <michalmatoga@gmail.com>
2025-06-23 10:53:49 +0200

    chore: add autocmd for auto-saving and keymap to close terminal

--- .config/lazyvim/lua/config/keymaps.lua
+++ .config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local map = vim.keymap.set
+
+map("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
