0e132a1ab175835c97bc75a055e997471481b13f
michmato <michalmatoga@gmail.com>
2025-06-24 08:39:34 +0200

    chore: refactor leader key assignment to use local variable for clarity

--- .config/lazyvim/lua/config/options.lua
+++ .config/lazyvim/lua/config/options.lua
@@ -2,8 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local g = vim.g
 
 opt.swapfile = false
 opt.wrap = true
 
-vim.g.mapleader = ";"
+g.mapleader = ";"
