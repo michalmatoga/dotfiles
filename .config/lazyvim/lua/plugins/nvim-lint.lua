e99ff142e18fa2fd04e6b3397311ca9edbfccb1b
michmato <michalmatoga@gmail.com>
2025-06-30 12:03:00 +0200

    chore: remove nvim-lint configuration from LazyVim setup

--- .config/lazyvim/lua/plugins/nvim-lint.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "mfussenegger/nvim-lint",
-  event = "LazyFile",
-  opts = {
-    linters_by_ft = {
-      markdown = { "markdownlint-cli2 --config ~/.markdownlintrc" },
-    },
-  },
-}
