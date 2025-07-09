43d1067bceddaefef794920bdee0468dfcd949a6
michmato <michalmatoga@gmail.com>
2025-07-09 15:11:27 +0200

    chore: disable aw-watcher plugin in LazyVim configuration

--- .config/lazyvim/lua/plugins/aw-watcher.lua
+++ .config/lazyvim/lua/plugins/aw-watcher.lua
@@ -1,10 +1,13 @@
 return {
   {
     "lowitea/aw-watcher.nvim",
+    enabled = false,
     opts = { -- required, but can be empty table: {}
       -- add any options here
       -- for example:
       aw_server = {
+        -- ip route | awk '/default/ {print $3; exit}'
+        -- netsh.exe interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=5600 connectaddress=127.0.0.1 connectport=5600
         host = "172.25.32.1",
         port = 5600,
       },
