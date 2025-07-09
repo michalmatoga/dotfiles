7ce4f9f0d39b8dafaca22b084c666647cfd340f9
michmato <michalmatoga@gmail.com>
2025-07-09 14:41:08 +0200

    chore: add initial configuration for aw-watcher.nvim plugin

---
+++ .config/lazyvim/lua/plugins/aw-watcher.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "lowitea/aw-watcher.nvim",
+    opts = { -- required, but can be empty table: {}
+      -- add any options here
+      -- for example:
+      aw_server = {
+        host = "172.25.32.1",
+        port = 5600,
+      },
+    },
+  },
+}
