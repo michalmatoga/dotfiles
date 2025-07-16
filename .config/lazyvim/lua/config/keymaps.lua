6bdecb43754ef40764e2ed3c07d5ee1388f63fc3
michmato <michalmatoga@gmail.com>
2025-07-16 10:35:56 +0200

    chore: refactor keymaps and add nvim-cmp configuration with luasnip

--- .config/lazyvim/lua/config/keymaps.lua
+++ .config/lazyvim/lua/config/keymaps.lua
@@ -2,24 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-local ls = require("luasnip")
-
 local map = vim.keymap.set
 
 map("i", "jj", "<Esc>")
-
-map({ "i" }, "<C-K>", function()
-  ls.expand()
-end, { silent = true })
-map({ "i", "s" }, "<C-L>", function()
-  ls.jump(1)
-end, { silent = true })
-map({ "i", "s" }, "<C-J>", function()
-  ls.jump(-1)
-end, { silent = true })
-
-map({ "i", "s" }, "<C-E>", function()
-  if ls.choice_active() then
-    ls.change_choice(1)
-  end
-end, { silent = true })
