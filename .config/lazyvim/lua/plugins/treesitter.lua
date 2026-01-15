46fe529850d81350c58792982842e2ac2eb866cd
michmato <michalmatoga@gmail.com>
2026-01-15 10:05:59 +0100

    chore: Add Treesitter plugin configuration with commit pinning

---
+++ .config/lazyvim/lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    -- Pin to f7955203: commit 8cdffc6d added "tab" to vim query but parser
+    -- doesn't support it yet. See: https://github.com/nvim-treesitter/nvim-treesitter
+    -- Remove this pin once vim parser is updated with :tab support.
+    commit = "f7955203bb16eed15f9e0fbf7e39b86e0de96b47",
+  },
+}
