f5bd104d634610069b73c3bf19823418edc719d4
michmato <michalmatoga@gmail.com>
2026-02-05 16:28:38 +0100

    chore: Remove outdated markdownlint configuration and switch to none-ls

---
+++ .config/lazyvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = {
+            "--config",
+            os.getenv("HOME") .. "/.markdownlint.json",
+            "--",
+          },
+        },
+      },
+    },
+  },
+}
