4ce227c109f0ead471600ac94515d3a6b7c521d8
michmato <michalmatoga@gmail.com>
2026-02-18 15:19:27 +0100

    fix(wo): forward wo-run args
    Allow wo-run to pass flags like --verbose and --full-refresh to the TS entrypoint.

--- scripts/wo/wo-run.sh
+++ scripts/wo/wo-run.sh
@@ -66,4 +66,4 @@ export PATH="$HOME/.cache/npm/global/bin:$HOME/.nix-profile/bin:$HOME/.local/sta
 
 cd "$DOTFILES_DIR"
 
-exec npx --yes tsx "$DOTFILES_DIR/scripts/wo/main.ts"
+exec npx --yes tsx "$DOTFILES_DIR/scripts/wo/main.ts" "$@"
